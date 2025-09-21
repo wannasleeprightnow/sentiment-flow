b36b2f17cbb3d37222928a4239f0fb2896ba3e4d
neidadre <wannasleeprightnowww@gmail.com>
2025-09-22 01:52:15 +0500

    chore(deps): add ruff linter as dev dependency

--- backend/src/app/main/app.py
+++ backend/src/app/main/app.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 
 
 def create_app() -> FastAPI:
-  app = FastAPI(
-    title="sentiment-flow"
-  )
+    app = FastAPI(title="sentiment-flow")
 
-  return app
+    return app
