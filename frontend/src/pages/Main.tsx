75efaa0fba201a4d555cde5388badd5036c690c4
Matvey <korzaevmatvej@gmail.com>
2025-09-23 13:53:49 +0300

    feat: add dashboard page with stats, filters and accordion charts

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,3 +1,52 @@
+import MainStat from "../components/MainStat";
+import CustomAccordion from "../components/CustomAccordion";
+import ChartMain from "../components/ChartMain";
+import DataFilters from "../components/DataFilters";
+import { useState } from "react";
+import type { DateT } from "../types/dashboards";
+
 export function Main() {
-	return <h1>Главная страница</h1>
+	const [startDate, setStartDate] = useState<DateT | null>(null)
+	const [endDate, setEndDate] = useState<DateT | null>(null)
+
+	const handleChangeDate = (type: "start" | "end", value: DateT): void => {
+		switch(type) {
+			case ("start"): {
+				setStartDate(value)
+				break;
+			}
+			case ("end"): {
+				setEndDate(value)
+				break
+			}
+			default: break
+		}
+	}
+
+	const handleSaveFilters = () => {}
+
+	const accordionSections = [
+    {
+      id: "1",
+      title: "Графики",
+      subtitle: "Стаистика в графическом виде за выбранный период",
+      content: <>
+	  	<ChartMain />
+	  </>
+    },
+	{
+      id: "2",
+      title: "Статистика в тексте",
+      subtitle: "Какая то хуйня еще",
+      content: <>
+	  	выфвфвф
+	  </>
+    }
+  ];
+  
+	return <div className="flex flex-col gap-6">
+		<MainStat />
+		<DataFilters startDate={startDate} endDate={endDate} handleChangeDate={handleChangeDate} handleSaveFilters={handleSaveFilters} />
+		<CustomAccordion sections={accordionSections} />
+	</div>
 }
