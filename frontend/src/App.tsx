726c323ef5e39080983812bc51843508660d569a
Matvey <korzaevmatvej@gmail.com>
2025-09-23 12:31:34 +0300

    feat: add Headless UI, Heroicons and wrap Main route with MainLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import { Auth } from "./pages/Auth";
 import { Main } from "./pages/Main";
 
 export function App() {
-	return (
-		<Routes>
-			<Route element={<Auth />} path="/auth" />
-			<Route element={<Main />} path="/" />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route element={<Auth />} path="/auth" />
+      <Route element={<MainLayout />} path="/">
+        <Route element={<Main />} path="/" />
+      </Route>
+    </Routes>
+  );
 }
